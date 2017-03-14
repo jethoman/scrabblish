d299520ef40f44e098c20b3d7fb412f05b4537c6
Joel Thoman <jethoman@gmail.com>
2017-03-14 18:11:17 -0400

    Practice Controller setup

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,13 @@
 */
 Route::get('/letters', 'ScabblishController@index');
 Route::get('/letters/{letter?}','ScabblishController@view');
+if(config('app.env')=='local'){
+    Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+}
+/**
+* Practice
+*/
+Route::any('/practice/{n?}', 'PracticeController@index');
+
 
 Route::get('/', 'WelcomeController');
